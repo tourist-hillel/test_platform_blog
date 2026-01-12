6c72f0e88d09ef01310c97ccae9c4db5665dd2d6
Gena <genada89@gmail.com>
2026-01-12 20:58:06 +0200

    added model Like

--- blog/models.py
+++ blog/models.py
@@ -31,3 +31,9 @@ class Comment(models.Model):
     
     def __str__(self):
         return f'Comment by {self.author_name} on {self.post}'
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    data = models.DateTimeField(default=timezone.now)
