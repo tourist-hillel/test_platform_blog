71d42159d9932ae1e6e12e55ff072ca7cf2efceb
Gena <genada89@gmail.com>
2026-01-12 21:03:31 +0200

    class Like add Meta and str

--- blog/models.py
+++ blog/models.py
@@ -37,3 +37,9 @@ class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
     data = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        unique_together = ('user', 'post')
+
+    def __str__(self):
+        return f"{self.user} liked {self.post}"
